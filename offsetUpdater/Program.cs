1e9722aee323bc37a52e4f7bd4fe58a7f5eb3c8d
Fredy Ait <aitfredy@gmail.com>
2021-04-10 00:55:34 +0300

    Exception handling & a tad cleaner code

--- offsetUpdater/Program.cs
+++ offsetUpdater/Program.cs
@@ -8,8 +8,6 @@ namespace offsetUpdater
 {
     class Program
     {
-
-
         static void Main(string[] args)
         {
             Console.WriteLine("Checking for updated offsets...");
@@ -24,8 +22,18 @@ namespace offsetUpdater
             WebClient wc = new WebClient(); //Initializing WebClient
             var parser = new FileIniDataParser(); //Initializing .ini parser
 
-            File.WriteAllText(githubOffsetPath, wc.DownloadString("https://raw.githubusercontent.com/frk1/hazedumper/master/csgo.toml")); //Downloading all of the up-to-date offsets from github and saving them to a .ini file for writing
-            
+            try
+            {
+                File.WriteAllText(githubOffsetPath, wc.DownloadString("https://raw.githubusercontent.com/frk1/hazedumper/master/csgo.toml")); //Downloading all of the up-to-date offsets from github and saving them to a .ini file for writing
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+
             File.WriteAllText(textLocalOffsetPath, parser.ReadFile(localOffsetPath).ToString()); //Simply saving Offsets.ini as a .txt for comparison
             File.WriteAllText(textGithubOffsetPath, parser.ReadFile(githubOffsetPath).ToString()); //Simply saving GithubOffsets.ini as a .txt for comparison
 
@@ -43,12 +51,20 @@ namespace offsetUpdater
                 {
                     Console.WriteLine(""); //Extra line to seperate the user's answer from the program's messages
                     Console.WriteLine("Overwriting offsets...");
-                    File.WriteAllText(localOffsetPath, wc.DownloadString("https://raw.githubusercontent.com/frk1/hazedumper/master/csgo.toml")); //Downloading all of the up-to-date offsets from github and saving them to the original .ini file for use
+
+                    try
+                    {
+                        File.WriteAllText(localOffsetPath, wc.DownloadString("https://raw.githubusercontent.com/frk1/hazedumper/master/csgo.toml")); //Downloading all of the up-to-date offsets from github and saving them to the original .ini file for use
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine("Error: " + e);
+                        Console.ReadLine();
+                        Environment.Exit(0);
+                    }
 
                     Console.WriteLine("Deleting temporary comparison files...");
-                    File.Delete(textGithubOffsetPath);
-                    File.Delete(textLocalOffsetPath);
-                    File.Delete(githubOffsetPath);
+                    FileDetetion();
 
                     Console.WriteLine("Everything is up-to-date!");
                     //Console.ReadLine(); for debugging purposes later
@@ -56,9 +72,7 @@ namespace offsetUpdater
                 else
                 {
                     Console.WriteLine("Deleting temporary comparison files...");
-                    File.Delete(textGithubOffsetPath);
-                    File.Delete(textLocalOffsetPath);
-                    File.Delete(githubOffsetPath);
+                    FileDetetion();
 
                     Console.WriteLine("Offsets left unupdated.");
                     //Console.ReadLine(); for debugging purposes later
@@ -67,9 +81,7 @@ namespace offsetUpdater
             else
             {
                 Console.WriteLine("Deleting temporary comparison files...");
-                File.Delete(textGithubOffsetPath);
-                File.Delete(textLocalOffsetPath);
-                File.Delete(githubOffsetPath);
+                FileDetetion();
 
                 Console.WriteLine("Everything is up-to-date!");
                 //Console.ReadLine(); for debugging purposes later
@@ -82,6 +94,26 @@ namespace offsetUpdater
             bool equalOrNot = File.ReadAllLines(file1).SequenceEqual(File.ReadAllLines(file2)); //Compares the local file's offsets to github's offsets
 
             return equalOrNot;
+        }
+
+        static void FileDetetion()
+        {
+            string githubOffsetPath = "C:\\Mediocre\\GithubOffsets.ini";
+            string textLocalOffsetPath = "C:\\Mediocre\\Offsets.txt";
+            string textGithubOffsetPath = "C:\\Mediocre\\GithubOffsets.txt";
+
+            try
+            {
+                File.Delete(textGithubOffsetPath);
+                File.Delete(textLocalOffsetPath);
+                File.Delete(githubOffsetPath);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error: " + e);
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
         }
     }
 }
