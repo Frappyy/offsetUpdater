74043a41a9bee41ae455ad3f2b45f3ee53f36b9a
Fredy Ait <aitfredy@gmail.com>
2021-04-15 09:20:14 +0300

    Rewrote everything

--- offsetUpdater/Definitions.cs
+++ offsetUpdater/Definitions.cs
@@ -1,10 +1,18 @@
-﻿namespace offsetUpdater
+﻿using System;
+using System.IO;
+
+namespace offsetUpdater
 {
     class Definitions
     {
         //Seperate class file for cleaner code & future proofing
         public string localOffsetPath = "C:\\Mediocre\\Offsets.ini";
         public string githubOffsetPath = "C:\\Mediocre\\GithubOffsets.ini";
+        public string steamInfPath = "G:\\Games\\Steam Library\\steamapps\\common\\Counter-Strike Global Offensive\\csgo\\steam.inf";
+        public string csgoVersionPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\OffsetUpdater\\csgoVersion.txt";
+        public string csgoVersionFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\OffsetUpdater";
+
+        public string hazeDumperLink = "https://raw.githubusercontent.com/frk1/hazedumper/master/csgo.toml";
 
         public string textLocalOffsetPath = "C:\\Mediocre\\Offsets.txt";
         public string textGithubOffsetPath = "C:\\Mediocre\\GithubOffsets.txt";
--- offsetUpdater/Program.cs
+++ offsetUpdater/Program.cs
@@ -3,6 +3,11 @@ using System.IO;
 using IniParser;
 using System.Net;
 using System.Linq;
+using IniParser.Model;
+using System.Threading;
+using System.Diagnostics;
+using System.IO.Compression;
+using System.Text.RegularExpressions;
 
 namespace offsetUpdater
 {
@@ -12,14 +17,42 @@ namespace offsetUpdater
         {
             WebClient wc = new WebClient();
             var parser = new FileIniDataParser();
-            Definitions def = new Definitions(); 
+            Definitions def = new Definitions();
+
+            Console.WriteLine("Checking for version file...");
+            CSGOVersionPathGen();
 
             try
             {
-                //Downloading all of the up-to-date offsets from github and saving them to a .ini file for writing
-                File.WriteAllText(def.githubOffsetPath, wc.DownloadString("https://raw.githubusercontent.com/frk1/hazedumper/master/csgo.toml"));
-                File.WriteAllText(def.textLocalOffsetPath, parser.ReadFile(def.localOffsetPath).ToString()); //Simply saving Offsets.ini as a .txt for comparison
-                File.WriteAllText(def.textGithubOffsetPath, parser.ReadFile(def.githubOffsetPath).ToString()); //Simply saving GithubOffsets.ini as a .txt for comparison            
+                if(!parser.ReadFile(def.steamInfPath).ToString().Contains(File.ReadAllText(def.csgoVersionPath)))
+                {
+                    Console.WriteLine("Updating offsets...\n");
+                    File.WriteAllText(def.githubOffsetPath, wc.DownloadString(def.hazeDumperLink));
+                    File.WriteAllText(def.textLocalOffsetPath, parser.ReadFile(def.localOffsetPath).ToString()); //Simply saving Offsets.ini as a .txt for comparison
+                    File.WriteAllText(def.textGithubOffsetPath, parser.ReadFile(def.githubOffsetPath).ToString()); //Simply saving GithubOffsets.ini as a .txt for comparison
+
+                    Console.WriteLine("Looks like CS:GO has updated and your offsets are outdated. Would you like to update them now?");
+                    Console.WriteLine("[Y] yes [N] no");
+
+                    if (Console.ReadLine().ToUpper() == "Y" || Console.ReadLine().ToUpper() == "YE" || Console.ReadLine().ToUpper() == "YES" && !DiffCheck(def.textLocalOffsetPath, def.textGithubOffsetPath)) //If "answer" equals to 'Y' or "YE" or "YES"
+                    {
+                        //Downloading all of the up-to-date offsets from github and saving them to a .ini file
+                        File.WriteAllText(def.localOffsetPath, wc.DownloadString(def.hazeDumperLink));
+                        TerminationSequence("up to date!", true, false);
+                    }
+                    else if(Console.ReadLine().ToUpper() == "Y" || Console.ReadLine().ToUpper() == "YE" || Console.ReadLine().ToUpper() == "YES" && DiffCheck(def.textLocalOffsetPath, def.textGithubOffsetPath))
+                    {
+                        Console.WriteLine("CS:GO has been updated but the offsets retrieved from hazedumper haven't been. This requires manual offset dumping.");
+                        TerminationSequence("left outdated.", false, true);
+                    }
+                    else
+                    {
+                        TerminationSequence("left outdated.", false, true);
+                    }
+
+                    TerminationSequence("up to date!", true, false);
+                }
+                TerminationSequence("up to date!", false, false);
             }
             catch(Exception e)
             {
@@ -27,53 +60,8 @@ namespace offsetUpdater
                 Console.ReadLine();
                 Environment.Exit(0);
             }
-
-            Console.WriteLine("Checking for updated offsets...");
-
-            if(!DiffCheck(def.textLocalOffsetPath, def.textGithubOffsetPath)) //If there's a difference (false)
-            {
-                Console.WriteLine("Looks like your offsets are outdated. Would you like to update them now?");
-                Console.WriteLine("[Y] yes [N] no");
-                string answer = Console.ReadLine(); //Storing user's input into a string called "answer"
-
-                if(answer.ToUpper() == "Y" || answer.ToUpper() == "YE" || answer.ToUpper() == "YES") //If "answer" equals to 'Y' or "YES"
-                {
-                    Console.WriteLine(""); //Extra line to seperate the user's answer from the program's messages
-                    Console.WriteLine("Overwriting offsets...");
-
-                    try
-                    {
-                        //Downloading all of the up-to-date offsets from github and saving them to a .ini file for writing
-                        File.WriteAllText(def.localOffsetPath, wc.DownloadString("https://raw.githubusercontent.com/frk1/hazedumper/master/csgo.toml"));
-                    }
-                    catch(Exception e)
-                    {
-                        Console.WriteLine("Error: " + e);
-                        Console.ReadLine();
-                        Environment.Exit(0);
-                    }
-
-                    Console.WriteLine("Deleting temporary comparison files...");
-                    FileDetetion();
-
-                    Console.WriteLine("Everything is up-to-date!");
-                }
-                else
-                {
-                    Console.WriteLine("Deleting temporary comparison files...");
-                    FileDetetion();
-
-                    Console.WriteLine("Offsets left unupdated.");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Deleting temporary comparison files...");
-                FileDetetion();
-
-                Console.WriteLine("Everything is up-to-date!");
-            };
         }
+
         static bool DiffCheck(string file1, string file2)
         {
             bool equalOrNot = File.ReadAllLines(file1).SequenceEqual(File.ReadAllLines(file2)); //Compares the local file's offsets to github's offsets
@@ -81,21 +69,53 @@ namespace offsetUpdater
             return equalOrNot;
         }
 
-        static void FileDetetion()
+        static void TerminationSequence(string message, bool versionChange, bool pause)
         {
             Definitions def = new Definitions();
+            var parser = new FileIniDataParser();
 
+            if (versionChange)
+            {
+                Regex pattern = new Regex("\\d+(\\.\\d+)+");
+                Match m = pattern.Match(parser.ReadFile(def.steamInfPath).ToString());
+
+                File.WriteAllText(def.csgoVersionPath, m.Value);
+            }
+
+            Console.WriteLine("Deleting temporary comparison files...");
             try
             {
                 File.Delete(def.textGithubOffsetPath);
                 File.Delete(def.textLocalOffsetPath);
                 File.Delete(def.githubOffsetPath);
+
             }
             catch(Exception e)
             {
-                Console.WriteLine("Error: " + e);
+                Console.WriteLine("Error: " + e.Message);
                 Console.ReadLine();
                 Environment.Exit(0);
+            }
+            Console.WriteLine("Offsets are " + message);
+
+            if (pause)
+            {
+                Console.ReadLine();
+            }
+        }
+
+        static void CSGOVersionPathGen()
+        {
+            Definitions def = new Definitions();
+
+            if (!Directory.Exists(def.csgoVersionFolderPath))
+            {
+                Directory.CreateDirectory(def.csgoVersionFolderPath);
+
+                if (!File.Exists(def.csgoVersionPath))
+                {
+                    File.WriteAllText(def.csgoVersionPath, "1.1.1.1");
+                }
             }
         }
     }
